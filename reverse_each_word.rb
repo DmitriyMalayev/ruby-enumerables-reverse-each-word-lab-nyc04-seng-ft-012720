958f6d2a829e2f04fd03990f61b52c6531fbfafa
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2020-01-21 05:55:28 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,19 @@
+def reverse_each_word(sentence1)   
+  return_array = []  
+  
+    array1 = sentence1.split(" ") 
+    array1.each do |a| puts a.reverse 
+    return_array << a.reverse 
+  end 
+    return return_array.join(" ")
+    end    
+    
+def reverse_each_word(ok)   
+array1 = ok.split(" ")
+return_array = [] 
+    
+array1.collect do |a| puts a.reverse 
+return_array << a.reverse 
+  end 
+    return return_array.join(" ")
+    end      
