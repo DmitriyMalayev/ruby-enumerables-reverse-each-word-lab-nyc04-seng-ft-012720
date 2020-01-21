f17394fcd1868e1c740fa81cf90631d3e9a8124a
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2020-01-21 03:03:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,5 +2,9 @@ def reverse_each_word(string)
  string = string.split('')
  reversed_string = []
  
+ string.each do |char|
+  reversed_string.unshift(char)
+ end
+ 
  return reversed_string.join('')
 end
