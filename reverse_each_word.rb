f3f3091fc1fa135fc5cd2cde94673030f6618c9e
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2020-01-21 05:47:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -11,7 +11,7 @@ def reverse_each_word(sentence1)
     
     
     
-    def reverse_each_word(sentence1)   
+    def reverse_each_word(collect)   
     array1 = sentence1.split(" ")
     return_array = [] 
     
