2ed767dd419b59d0eeb8a9a215ff6752b58570bf
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2020-01-21 03:56:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,8 @@
-# 
-
 def reverse_each_word(string)  
 new = [] 
 new = string.split(" ")
 
 end
-
 
 new.each do |ok| 
   puts "#{ok.reverse}"
@@ -13,14 +10,11 @@ end
 
 new.join("") 
 
-
 # oppressed_workers = ["Dopey", "Sneezy", "Happy", "Angry", "Doc", "Lemonjello", "Sleepy"]
 
 # oppressed_workers.each do |oppressed_worker| 
 #     puts "#{oppressed_worker.capitalize} wants to start a union!"
 # end 
-
-
 
 # def sum_array(number_array) 
 #     total = 0 
