45693d319fbe97d4b3aefca638f26daac4d37970
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2020-01-21 05:11:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(words)   
     words.split(" ") << newWords   
-    newWords.each{|words| words.reverse}
+    newWords.each{|word| word.reverse}
   
 return newWords  
     end    
