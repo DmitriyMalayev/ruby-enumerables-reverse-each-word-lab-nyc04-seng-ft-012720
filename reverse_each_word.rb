73319b3e42864985ab020ddae0fed6e83a38e332
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2020-01-21 05:39:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,44 +1,10 @@
 def reverse_each_word(sentence1)   
-    sentence1.split(" ")   
-    sentence1.each do |a| puts a.reverse 
-  end 
-    sentence1.join(" ")  
+    array1 = sentence1.split(" ")
+    return_array = [] 
     
+    
+    array1.each do |a| puts a.reverse 
+    return_array << string.reverse 
+  end 
+    return return_array.join(" ")
     end    
-
-
-# ary = [1,2,3,4,5]
-# ary.each do |i|
-#   puts i
-# end
-
-# def reverse(string)
-# string = string.split('')
-# reversed_string = []
- 
-# string.each do |char|
-#   reversed_string.unshift(char)
-# end
- 
-# return reversed_string.join('')
-# end
-
-# .array = ["a", "b", "c"]
-# array.map{|string| string.upcase }
-
-# #=>["A", "B", "C"]
-
-
-# oppressed_workers = ["Dopey", "Sneezy", "Happy", "Angry", "Doc", "Lemonjello", "Sleepy"]
-
-# oppressed_workers.each do |oppressed_worker| 
-#     puts "#{oppressed_worker.capitalize} wants to start a union!"
-# end 
-
-# def sum_array(number_array) 
-#     total = 0 
-#     number_array.each{|num| total += num}
-#     total 
-# end 
-
-# sum_array([1, 2, 3]) #=> 6 
