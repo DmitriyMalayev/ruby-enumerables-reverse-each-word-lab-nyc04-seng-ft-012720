eff2154d29605395e52a699d34082326e8723cb0
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2020-01-21 03:43:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,12 +4,9 @@ def reverse_each_word(string)
 new = string.split(" ")
 end
 
+new 
 
 
-# sorted_member_list = bands.reduce({}) do |memo, (key, value)|
-#     memo[key] = value.sort 
-#     memo 
-# end 
-
-# p bands 
-# p sorted_member_list   
+sorted_member_list = new.reduce do |memo|
+  memo.reverse
+  end 
